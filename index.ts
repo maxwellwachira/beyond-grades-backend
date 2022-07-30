3af6ef2a6a8b3569f3390b1d43e491a74314a1dd
maxwellwachira <maxwellwachira67@gmail.com>
2022-07-31 01:00:17 +0300

    Added Swagger UI configurations

--- index.ts
+++ index.ts
@@ -1,14 +1,34 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
+
+
+import db from './src/config/database.config'; 
+import userRoutes from "./src/users/UserController";
 
 dotenv.config();
 
 const app: Express = express();
+
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+const dbAuthenticate = async () => {
+  try {
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+dbAuthenticate();
+
+app.use('/user', userRoutes);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);

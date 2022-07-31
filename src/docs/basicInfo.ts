0afe60ba72a5c80f32bf6b3c0fb6aa3b89efcaae
maxwellwachira <maxwellwachira67@gmail.com>
2022-07-31 14:53:05 +0300

    OpenAPI specification basic info

---
+++ src/docs/basicInfo.ts
@@ -0,0 +1,13 @@
+export const basiciInfo = {
+    openapi: "3.0.3", 
+    info: {
+        title: "Beyond Grades API Documentation", 
+        description: "Beyond Grades NodeJS REST API Documentation", 
+        version: "1.0.0", 
+        contact: {
+            name: "Maxwell Wachira", 
+            email: "maxwellwachira67@gmail.com",
+            url: "https://maxwellwachira.com",
+        },
+    },
+}

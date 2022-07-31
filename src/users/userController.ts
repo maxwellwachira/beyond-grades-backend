f922723beb72e07bdb5cbe1ceed9cbbcd4a8170a
maxwellwachira <maxwellwachira67@gmail.com>
2022-08-01 01:10:32 +0300

    User controller file

---
+++ src/users/userController.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+
+import { createUser, getUsers, getUser, updateUser, deleteUser } from './userService';
+import UserValidator from "./userValidator";
+import { validationMiddleware } from './userMiddlewares';
+import userValidator from './userValidator';
+
+const router = express.Router();
+
+//Create user
+router.post('/', UserValidator.checkCreateUser(), validationMiddleware, createUser);
+//Get all users
+router.get('/', userValidator.checkGetUsers(), validationMiddleware, getUsers);
+//Get one user
+router.get('/:id', userValidator.checkIdParam(), validationMiddleware, getUser);
+//Update user
+router.put('/:id', userValidator.checkIdParam(), validationMiddleware, updateUser);
+//Delete user
+router.delete('/:id', userValidator.checkIdParam(), validationMiddleware, deleteUser);
+
+
+
+
+
+export default router;
